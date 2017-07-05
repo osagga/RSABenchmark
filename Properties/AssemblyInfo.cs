cabd61d4f26a49158617c933ffaade682d31cdc4
Omar Sagga <osagga@outlook.com>
2017-07-05 16:53:06 -0400

    Cleaning

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Benchmark1")]
+[assembly: AssemblyTitle("RSA_Benchmark")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Benchmark1")]
+[assembly: AssemblyCompany("osagga")]
+[assembly: AssemblyProduct("RSA_Benchmark")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
